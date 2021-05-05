a1463e4f60f198191fae02ca4ff114544677b604
misaosyushi <tujitamisaki@gmail.com>
2021-05-05 14:51:18 +0900

    add api gateway

--- src/CsCdkSample/CsCdkSampleStack.cs
+++ src/CsCdkSample/CsCdkSampleStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
 using Amazon.CDK.AWS.Lambda;
 
 namespace CsCdkSample
@@ -7,11 +8,16 @@ namespace CsCdkSample
     {
         internal CsCdkSampleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            var hello = new Function(this, "HelloHandler", new FunctionProps
+            var helloLambda = new Function(this, "HelloHandler", new FunctionProps
             {
                 Runtime = Runtime.NODEJS_14_X,
                 Code = Code.FromAsset("lambda"),
                 Handler = "hello.handler"
+            });
+
+            new LambdaRestApi(this, "HelloApi", new LambdaRestApiProps
+            {
+                Handler = helloLambda
             });
         }
     }
