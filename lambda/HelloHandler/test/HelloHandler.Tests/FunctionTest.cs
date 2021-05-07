b2796a964e78bb8302bf3055c39bc78684858905
misaosyushi <tujitamisaki@gmail.com>
2021-05-07 09:16:00 +0900

    add unit test

--- lambda/HelloHandler/test/HelloHandler.Tests/FunctionTest.cs
+++ lambda/HelloHandler/test/HelloHandler.Tests/FunctionTest.cs
@@ -1,28 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Xunit;
-using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
-
-using HelloHandler;
 
 namespace HelloHandler.Tests
 {
     public class FunctionTest
     {
         [Fact]
-        public void TestToUpperFunction()
+        public void TestHelloCdkFunction()
         {
-
-            // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+            var apiGatewayProxyRequest = new APIGatewayProxyRequest();
+            var response = function.FunctionHandler(apiGatewayProxyRequest, context);
 
-            Assert.Equal("HELLO WORLD", upperCase);
+            Assert.Equal("Hello CDK!", response.Body);
+            Assert.Equal((int) HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
